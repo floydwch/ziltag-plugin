a97d636c65a1df23087f2e9348177854b94a6d48
floydsoft <floydsoft@gmail.com>
2015-08-06 09:38:17 +0800

    add gulpfile

---
+++ demo/gulpfile.babel.js
@@ -0,0 +1,19 @@
+import gulp from 'gulp';
+
+let browserSync = require('browser-sync');
+let reload = browserSync.reload;
+
+gulp.task('serve', () => {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        open: false
+    });
+
+    gulp.watch(
+        ['*.html', 'css/*.css', 'js/*.js'],
+        {cwd: 'app'},
+        reload
+    );
+});
