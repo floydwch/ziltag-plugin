8582cfa82ffd0e22dfceb37de946c0602b0cc33e
floydwch <floydwch@gmail.com>
2015-09-29 20:37:57 +0800

    update webpack

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-import ZiltagApp from './components/ziltag-app';
-
-
-React.render(<ZiltagApp></ZiltagApp>, document.getElementsByTagName('ziltagapp')[0]);
--- webpack.config.js
+++ webpack.config.js
@@ -5,31 +5,34 @@ import postcss_nesting from 'postcss-nesting';
 
 
 module.exports = {
-    devtool: 'eval',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
-        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-        './index' // Your appʼs entry point
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index' // Your appʼs entry point
     ],
     output: {
-        filename: 'ziltag-plugin.js',
-        path: path.join(__dirname, 'dist'),
-        publicPath: '/dist/'
+      filename: 'ziltag-plugin.js',
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/dist/'
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+      extensions: ['', '.js', '.jsx']
     },
     module: {
-        loaders: [
-            { test: /\.jsx?$/, exclude:  /(node_modules|vendor)/, loaders: ['react-hot', 'babel'] },
-            { test: /\.css$/, loader: 'style!css!postcss!cssnext' },
-            { test: /\.(png|jpg|eot)$/, loader: 'url' }
-        ]
+      loaders: [
+      { test: /\.jsx?$/, exclude:  /(node_modules|vendor)/, loaders: ['react-hot', 'babel'] },
+      { test: /\.css$/, loader: 'style!css!postcss!cssnext' },
+      { test: /\.(png|jpg|eot)$/, loader: 'url' }
+      ]
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.ProvidePlugin({
+        'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+      })
     ],
     postcss: () => {
-        return [postcss_nesting, cssnano];
+      return [postcss_nesting, cssnano];
     }
 };
