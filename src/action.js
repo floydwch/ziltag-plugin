e30563b78e7c9f328b541e117a5c6509ea1621f4
floydwch <floydwch@gmail.com>
2015-10-06 16:22:35 +0800

    refactor ziltag's fetching and updating

--- src/action.js
+++ src/action.js
@@ -1,49 +1,42 @@
-export function init_ziltag_map(bind_id, x, y, width, height) {
-  const action = { type: 'INIT_ZILTAG_MAP', map: {} };
-  action.map[bind_id] = {
-    x,
-    y,
-    width,
-    height
-  };
-  return action;
-}
-
-export function fetch_ziltags(bind_id, src) {
+export function activate_ziltag_map(x, y, width, height, src, href) {
+  const dev_token = 'd3d4b9';
   return dispatch => {
-    return fetch(`http://staging.ziltag.com/api/v1/ziltags/?src=${src}`)
+    return fetch(
+      `http://staging.ziltag.com/api/v1/ziltags/` +
+      `?token=${dev_token}&src=${src}&href=${href}`)
       .then(resp => resp.json())
       .then(json => {
-        const action = { type: 'FETCH_ZILTAGS', map: {} };
-        action.map[bind_id] = {
-          map_id: json.map,
-          ziltags: json.ziltags
+        const { map: map_id, ziltags } = json;
+        const action = {
+          type: 'ACTIVATE_ZILTAG_MAP',
+          x,
+          y,
+          width,
+          height,
+          map_id,
+          ziltags
         };
         dispatch(action);
       });
   };
 }
 
-export function activate_ziltag_map(map_id) {
-  return { type: 'ACTIVATE_ZILTAG_MAP', map_id };
-}
-
 export function deactivate_ziltag_map() {
   return { type: 'DEACTIVATE_ZILTAG_MAP' };
 }
 
-export function open_ziltag_preview(map_id, ziltag_id) {
-  return { type: 'OPEN_ZILTAG_PREVIEW', map_id, ziltag_id };
+export function activate_ziltag_preview(map_id, ziltag_id) {
+  return { type: 'ACTIVATE_ZILTAG_PREVIEW', map_id, ziltag_id };
 }
 
-export function close_ziltag_preview() {
-  return { type: 'CLOSE_ZILTAG_PREVIEW' };
+export function deactivate_ziltag_preview() {
+  return { type: 'DEACTIVATE_ZILTAG_PREVIEW' };
 }
 
-export function open_ziltag_reader(map_id, ziltag_id) {
-  return { type: 'OPEN_ZILTAG_READER', map_id, ziltag_id };
+export function activate_ziltag_reader(map_id, ziltag_id) {
+  return { type: 'ACTIVATE_ZILTAG_READER', map_id, ziltag_id };
 }
 
-export function close_ziltag_reader() {
-  return { type: 'CLOSE_ZILTAG_READER' };
+export function deactivate_ziltag_reader() {
+  return { type: 'DEACTIVATE_ZILTAG_READER' };
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,33 +1,11 @@
 import { combineReducers } from 'redux';
 
 
-function ziltag_maps(state={}, action) {
-  const bind_id = Object.keys(action.map || {})[0];
-  let next_state = Object.assign({}, state);
-
-  next_state[bind_id] = next_state[bind_id] || {};
-
-  switch (action.type) {
-    case 'INIT_ZILTAG_MAP':
-      next_state[bind_id].x = action.map[bind_id].x;
-      next_state[bind_id].y = action.map[bind_id].y;
-      next_state[bind_id].width = action.map[bind_id].width;
-      next_state[bind_id].height = action.map[bind_id].height;
-      return next_state;
-    case 'FETCH_ZILTAGS':
-      next_state[bind_id].map_id = action.map[bind_id].map_id;
-      next_state[bind_id].ziltags = action.map[bind_id].ziltags;
-      return next_state;
-    default:
-      return state;
-  }
-}
-
-function working_ziltag_map(state={}, action) {
+function ziltag_map(state={}, action) {
   switch (action.type) {
     case 'ACTIVATE_ZILTAG_MAP':
-      const { map_id } = action;
-      return { map_id };
+      const { x, y, width, height, map_id, ziltags } = action;
+      return { x, y, width, height, map_id, ziltags };
     case 'DEACTIVATE_ZILTAG_MAP':
       return {};
     default:
@@ -35,12 +13,12 @@ function working_ziltag_map(state={}, action) {
   }
 }
 
-function working_ziltag_preview(state={}, action) {
+function ziltag_preview(state={}, action) {
   switch (action.type) {
-    case 'OPEN_ZILTAG_PREVIEW':
+    case 'ACTIVATE_ZILTAG_PREVIEW':
       const { map_id, ziltag_id } = action;
       return { map_id, ziltag_id };
-    case 'CLOSE_ZILTAG_PREVIEW':
+    case 'DEACTIVATE_ZILTAG_PREVIEW':
       return {};
     default:
       return state;
@@ -49,10 +27,10 @@ function working_ziltag_preview(state={}, action) {
 
 function ziltag_reader(state={}, action) {
   switch (action.type) {
-    case 'OPEN_ZILTAG_READER':
+    case 'ACTIVATE_ZILTAG_READER':
       const { map_id, ziltag_id } = action;
       return { map_id, ziltag_id };
-    case 'CLOSE_ZILTAG_READER':
+    case 'DEACTIVATE_ZILTAG_READER':
       return {};
     default:
       return state;
@@ -60,9 +38,8 @@ function ziltag_reader(state={}, action) {
 }
 
 const ZiltagAppReducer = combineReducers({
-  ziltag_maps,
-  working_ziltag_map,
-  working_ziltag_preview,
+  ziltag_map,
+  ziltag_preview,
   ziltag_reader
 });
 
