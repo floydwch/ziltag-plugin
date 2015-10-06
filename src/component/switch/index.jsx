945d75e0e8582e4f4124632324d947d9ae477b6f
floydwch <floydwch@gmail.com>
2015-10-06 16:24:44 +0800

    update Switch for new process

--- src/component/switch/index.jsx
+++ src/component/switch/index.jsx
@@ -5,8 +5,10 @@ require('./index.css');
 
 class Switch extends React.Component {
   render() {
-    const { map_id, actions } = this.props;
+    const { map_id, x, y, actions } = this.props;
+    const style = { top: y, left: x };
     return <div
+      style={style}
       className='ziltag-switch'
       onClick={() => actions.open_ziltag_reader(map_id)}
     >
