0481e063e6d0e57533c1c4ae6b116538641f05c4
floydwch <floydwch@gmail.com>
2015-11-01 20:52:02 +0800

    format

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,28 +8,28 @@ module.exports = {
   entry: [
     './src/index'
     ],
-    output: {
-      filename: 'ziltag-plugin.js',
-      path: path.join(__dirname, 'dist'),
-      publicPath: '/dist/'
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx']
-    },
-    module: {
-      loaders: [
-        { test: /\.jsx?$/, exclude:  /(node_modules|vendor)/, loader: 'babel' },
-        { test: /\.css$/, loader: 'style!css!postcss!cssnext' },
-        { test: /\.(png|jpg|eot)$/, loader: 'url' },
-        { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader' }
-      ]
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-        'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-      })
-    ],
-    postcss: () => {
-      return [postcss_nesting, cssnano];
-    }
+  output: {
+    filename: 'ziltag-plugin.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude:  /(node_modules|vendor)/, loader: 'babel' },
+      { test: /\.css$/, loader: 'style!css!postcss!cssnext' },
+      { test: /\.(png|jpg|eot)$/, loader: 'url' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+    })
+  ],
+  postcss: () => {
+    return [postcss_nesting, cssnano];
+  }
 };
