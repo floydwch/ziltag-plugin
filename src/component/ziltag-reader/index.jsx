c0713adf4258e3d6c602d75e40f3ca4869616dca
Jian Weihang <tonytonyjan@gmail.com>
2015-12-17 02:10:05 +0800

    fix path

--- src/component/ziltag-reader/index.jsx
+++ src/component/ziltag-reader/index.jsx
@@ -5,10 +5,10 @@ require('./index.css');
 
 class ZiltagReader extends React.Component {
   render() {
-    const { map_id, ziltag_id, actions } = this.props;
-    const src = `${SERVER_ADDRESS}/ziltags/` +
-    `${map_id}/${ziltag_id || ''}`;
-    const style = { zIndex: MAX_Z_INDEX };
+    const { map_id, ziltag_id, actions } = this.props
+    const path = ziltag_id ? `ziltags/${ziltag_id}` : `ziltag_maps/${map_id}`
+    const src = `${SERVER_ADDRESS}/${path}`
+    const style = { zIndex: MAX_Z_INDEX }
 
     return  <div
       style={style}
