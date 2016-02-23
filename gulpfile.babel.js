284a8674002518f61c2246519628dd6a443e6dd0
floydwch <floydwch@gmail.com>
2016-02-23 18:23:21 +0800

    style: remove semicolons

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,35 +1,35 @@
-import gulp from 'gulp';
-import del from 'del';
-import webpack from 'webpack';
-import webpackStream from 'webpack-stream';
-import WebpackDevServer from 'webpack-dev-server';
-import ghPages from 'gulp-gh-pages';
-import env from 'gulp-env';
+import gulp from 'gulp'
+import del from 'del'
+import webpack from 'webpack'
+import webpackStream from 'webpack-stream'
+import WebpackDevServer from 'webpack-dev-server'
+import ghPages from 'gulp-gh-pages'
+import env from 'gulp-env'
 
-import webpack_config from './webpack.config.babel';
-import common_define from './src/common_define';
+import webpack_config from './webpack.config.babel'
+import common_define from './src/common_define'
 
 
 gulp.task('clean', (done) => {
-  del(['dist']);
-  del(['demo/app/dist'], done);
-});
+  del(['dist'])
+  del(['demo/app/dist'], done)
+})
 
 gulp.task('serve', ['clean'], () => {
-  const dev_webpack_config = Object.assign({}, webpack_config);
-  dev_webpack_config.devtool = 'source-map';
-  dev_webpack_config.debug = true;
+  const dev_webpack_config = Object.assign({}, webpack_config)
+  dev_webpack_config.devtool = 'source-map'
+  dev_webpack_config.debug = true
   dev_webpack_config.entry = [...dev_webpack_config.entry,
     'webpack-dev-server/client?http://localhost:4000',
     'webpack/hot/only-dev-server'
-  ];
+  ]
   dev_webpack_config.plugins = [...dev_webpack_config.plugins,
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin(Object.assign({}, common_define, {
       SERVER_ADDRESS: JSON.stringify('http://localhost:2000'),
       API_ADDRESS: JSON.stringify('http://localhost:3000')
     }))
-  ];
+  ]
 
   new WebpackDevServer(webpack(dev_webpack_config), {
     publicPath: dev_webpack_config.output.publicPath,
@@ -38,19 +38,19 @@ gulp.task('serve', ['clean'], () => {
     historyApiFallback: true
   }).listen(4000, 'localhost', (err, result) => {
     if(err) {
-      console.log(err);
+      console.log(err)
     }
-    console.log('Listening at localhost:4000');
-  });
-});
+    console.log('Listening at localhost:4000')
+  })
+})
 
 gulp.task('build:staging', (cb) => {
   env({
     vars:{
       NODE_ENV: 'production'
     }
-  });
-  const pro_webpack_config = Object.assign({}, webpack_config);
+  })
+  const pro_webpack_config = Object.assign({}, webpack_config)
   pro_webpack_config.plugins = [...pro_webpack_config.plugins,
     new webpack.DefinePlugin(Object.assign({}, common_define, {
       SERVER_ADDRESS:
@@ -60,21 +60,21 @@ gulp.task('build:staging', (cb) => {
     })),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
-  ];
+  ]
 
   return gulp.src('index.js')
   .pipe(webpackStream(pro_webpack_config, webpack))
   .pipe(gulp.dest('dist/staging'))
-  .pipe(gulp.dest('demo/app/dist/staging'));
-});
+  .pipe(gulp.dest('demo/app/dist/staging'))
+})
 
 gulp.task('build:production', (cb) => {
   env({
     vars:{
       NODE_ENV: 'production'
     }
-  });
-  const pro_webpack_config = Object.assign({}, webpack_config);
+  })
+  const pro_webpack_config = Object.assign({}, webpack_config)
   pro_webpack_config.plugins = [...pro_webpack_config.plugins,
     new webpack.DefinePlugin(Object.assign({}, common_define, {
       SERVER_ADDRESS:
@@ -84,19 +84,19 @@ gulp.task('build:production', (cb) => {
     })),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
-  ];
+  ]
 
   return gulp.src('index.js')
   .pipe(webpackStream(pro_webpack_config, webpack))
   .pipe(gulp.dest('dist/production'))
-  .pipe(gulp.dest('demo/app/dist/production'));
-});
+  .pipe(gulp.dest('demo/app/dist/production'))
+})
 
-gulp.task('build', ['clean', 'build:staging', 'build:production']);
+gulp.task('build', ['clean', 'build:staging', 'build:production'])
 
 gulp.task('deploy', ['build'], () => {
   return gulp.src('demo/app/*/*/*')
-  .pipe(ghPages());
-});
+  .pipe(ghPages())
+})
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build'])
--- src/component/ziltag/index.jsx
+++ src/component/ziltag/index.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from 'react'
 
-require('./index.css');
+require('./index.css')
 
 
 class Ziltag extends React.Component {
   render() {
-    const { ziltag_id, map_id, x, y, actions } = this.props;
-    const radius = 6;
+    const { ziltag_id, map_id, x, y, actions } = this.props
+    const radius = 6
     const style = {
       top: y - radius,
       left: x - radius,
       zIndex: MAX_Z_INDEX - 3
-    };
+    }
 
     return <div
       className='ziltag-ziltag'
       style={style}
       onClick={() => {
-        actions.deactivate_ziltag_map();
-        actions.activate_ziltag_reader(map_id, ziltag_id);
+        actions.deactivate_ziltag_map()
+        actions.activate_ziltag_reader(map_id, ziltag_id)
       }}
       onMouseEnter={() => actions.activate_ziltag_preview(map_id, ziltag_id)}
       onMouseLeave={actions.deactivate_ziltag_preview}
-    ></div>;
+    ></div>
   }
 }
 
-export default Ziltag;
+export default Ziltag
