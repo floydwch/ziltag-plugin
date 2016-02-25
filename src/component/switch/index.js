da05916688524518ca68a898220e2d70d2fdf60c
floydwch <floydwch@gmail.com>
2016-02-26 04:36:16 +0800

    style: remove semicolons

--- src/component/switch/index.js
+++ src/component/switch/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-require('./index.css');
+require('./index.css')
 
 
 class Switch extends React.Component {
   render() {
-    const { map_id, x, y, actions } = this.props;
-    const style = { top: y, left: x, zIndex: MAX_Z_INDEX - 1 };
+    const { map_id, x, y, actions } = this.props
+    const style = { top: y, left: x, zIndex: MAX_Z_INDEX - 1 }
     return <div
       style={style}
       className='ziltag-switch'
@@ -14,13 +14,13 @@ class Switch extends React.Component {
       onMouseLeave={
         (e) => {
           if (e.nativeEvent.relatedTarget.nodeName != 'IMG') {
-            actions.deactivate_ziltag_map();
+            actions.deactivate_ziltag_map()
           }
         }
       }
     >
-    </div>;
+    </div>
   }
 }
 
-export default Switch;
+export default Switch
--- src/component/ziltag-map/index.js
+++ src/component/ziltag-map/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-import Ziltag from '../ziltag';
-import ZiltagPreview from '../ziltag-preview';
-import Switch from '../switch';
+import Ziltag from '../ziltag'
+import ZiltagPreview from '../ziltag-preview'
+import Switch from '../switch'
 
-require('./index.css');
+require('./index.css')
 
 
 class ZiltagMap extends React.Component {
@@ -14,16 +14,16 @@ class ZiltagMap extends React.Component {
       ziltags,
       ziltag_preview,
       actions
-    } = this.props;
+    } = this.props
 
     const style = {
       top: y,
       left: x
-    };
+    }
 
-    const tag_ziltags = [];
+    const tag_ziltags = []
     for (let i = 0; i < (ziltags || []).length; ++i) {
-      const ziltag = ziltags[i];
+      const ziltag = ziltags[i]
 
       tag_ziltags.push(
         <Ziltag
@@ -35,18 +35,18 @@ class ZiltagMap extends React.Component {
           y={ziltag.y * height}
           is_focused={ziltag.id && ziltag_preview.ziltag_id == ziltag.id}
         />
-      );
+      )
 
       if (ziltag.x >= 0.5) {
-        var side = 'right';
+        var side = 'right'
       } else {
-        var side = 'left';
+        var side = 'left'
       }
 
       if (ziltag.y * height < 30) {
-        side = 'upper-' + side;
+        side = 'upper-' + side
       } else if (height - ziltag.y * height < 30) {
-        side = 'lower-' + side;
+        side = 'lower-' + side
       }
 
       if (ziltag.id && ziltag_preview.ziltag_id == ziltag.id) {
@@ -56,11 +56,11 @@ class ZiltagMap extends React.Component {
           side={side}
           content={ziltag.content}
           usr={ziltag.usr}
-        />;
+        />
       }
     }
 
-    const switch_width = 52;
+    const switch_width = 52
 
     return <div
       style={style}
@@ -69,8 +69,8 @@ class ZiltagMap extends React.Component {
       <Switch map_id={map_id} x={width - switch_width} y={0} actions={actions}/>
       {tag_ziltags}
       {tag_ziltag_preview}
-    </div>;
+    </div>
   }
 }
 
-export default ZiltagMap;
+export default ZiltagMap
