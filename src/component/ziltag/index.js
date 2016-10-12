7bbab177d6944fdb4ef88a17bf6e1c961c7d8f48
floydwch <floydwch@gmail.com>
2016-10-12 13:50:20 +0800

    refactor: refactor Ziltag to a functional component

--- src/component/ziltag/index.js
+++ src/component/ziltag/index.js
@@ -3,20 +3,13 @@ import React from 'react'
 require('./index.css')
 
 
-class Ziltag extends React.Component {
-  render() {
-    const {
-      style, onClick, onMouseEnter, onMouseLeave
-    } = this.props
-
-    return <div
-      className='ziltag-ziltag'
-      style={style}
-      onClick={onClick}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    ></div>
-  }
-}
-
-export default Ziltag
+export default ({style, onClick, onMouseEnter, onMouseLeave}) => (
+  <div
+    className='ziltag-ziltag'
+    style={style}
+    onClick={onClick}
+    onMouseEnter={onMouseEnter}
+    onMouseLeave={onMouseLeave}
+  >
+  </div>
+)
